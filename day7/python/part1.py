ee259c70f30de074bf577c3e99fbe232d6122a69
joaomcteixeira <joaomcteixeira@gmail.com>
2023-12-18 01:01:26 +0100

    day 7

---
+++ day7/python/part1.py
@@ -0,0 +1,36 @@
+import os
+import sys
+from collections import Counter
+from pathlib import Path
+
+card_map = {c:i for i, c in enumerate('AKQJT98765432', start=1)}
+type_map = {
+    (5,): 1,
+    (4, 1): 2,
+    (3, 2): 3,
+    (3, 1, 1): 4,
+    (2, 2, 1): 5,
+    (2, 1, 1, 1): 6,
+    (1, 1, 1, 1, 1): 7,
+    }
+
+
+def sort(s):
+    a = type_map[tuple(sorted(Counter(s).values(), reverse=True))]
+    b = tuple(card_map[c] for c in s)
+    return (a,) + b
+
+
+def main(input_):
+    lines = Path(input_).read_text().split(os.linesep)
+    lines.sort(key=lambda x: sort(x[:5]), reverse=True)
+    total = 0
+    for i, line in enumerate(lines, start=1):
+        hand, bid = line.split()
+        total += i * int(bid)
+
+    return total
+
+
+if __name__ == '__main__':
+    print(main(sys.argv[1]))
